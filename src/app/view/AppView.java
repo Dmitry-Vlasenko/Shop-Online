7b36e8734b1777954c846df1fb523f885b39df25
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-02 17:13:47 +0200

    add view for app

---
+++ src/app/view/AppView.java
@@ -0,0 +1,34 @@
+package app.view;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AppView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String[] getData() {
+        scanner.useLocale(Locale.ENGLISH);
+        System.out.print("Enter buyer name: ");
+        String name = scanner.nextLine().trim();
+        System.out.print("Enter buyer phone: ");
+        String phone = scanner.nextLine().trim();
+        System.out.print("Enter product count: ");
+        String quantity = scanner.nextLine().trim();
+        System.out.print("Enter product price, USD: ");
+        String price = scanner.nextLine().trim();
+        System.out.println("Deliver product? + or -");
+        String deliver = scanner.nextLine().trim();
+        String address = "";
+        if (deliver.equals("+")){
+            System.out.println("Address for delivered?: ");
+            address = scanner.nextLine().trim();
+        }
+        scanner.close();
+        return new String[]{name, phone, quantity, price, deliver, address};
+    }
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
