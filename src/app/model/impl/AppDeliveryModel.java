fe3d13cfad44ed3478046b66a8513ade57da2b70
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-02 19:53:14 +0200

    add Calculation

---
+++ src/app/model/impl/AppDeliveryModel.java
@@ -0,0 +1,14 @@
+package app.model.impl;
+
+import app.entity.Delivery;
+import app.entity.Purchase;
+import app.model.AppModel;
+
+public class AppDeliveryModel implements AppModel {
+    @Override
+    public double calcPayment(Purchase purchase) {
+        double cost = purchase.getQuota() * purchase.getPrice();
+        System.out.println(cost);
+        return cost + (cost * 5/100);
+    }
+}
