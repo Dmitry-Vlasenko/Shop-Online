6537b57311e6c459e03b20a0fb68b25190897ead
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-02 20:03:49 +0200

    Write logic for app controller

--- src/app/controller/AppController.java
+++ src/app/controller/AppController.java
@@ -3,7 +3,10 @@ package app.controller;
 import app.entity.Buyer;
 import app.entity.Delivery;
 import app.entity.Purchase;
+import app.model.impl.AppBaseModel;
+import app.model.impl.AppDeliveryModel;
 import app.utils.Convert;
+import app.utils.Rounder;
 import app.view.AppView;
 
 public class AppController {
@@ -13,10 +16,23 @@ public class AppController {
 
     public void getPayment() {
         String[] data = view.getData();
+        String output;
         Buyer buyer = getBuyer(data);
         Purchase purchase = getPurchase(data);
         Delivery delivery = getDelivery(data);
-        System.out.println(delivery.getDeliveryPost());
+        if (delivery.getDeliveryPost()) {
+            AppDeliveryModel model = new AppDeliveryModel();
+            String payment = Rounder.roundValue(model.calcPayment(purchase));
+            output = "\nBuyer: " + buyer.getName() + ", " + buyer.getPhone() +
+                    "\nPayment is " + CURRENCY + " " + payment + "\nDelivery address: " + delivery.getAddress();
+            view.getOutput(output);
+        } else {
+            AppBaseModel model = new AppBaseModel();
+            String payment = Rounder.roundValue(model.calcPayment(purchase));
+            output = "\nBuyer: " + buyer.getName() + ", " + buyer.getPhone() +
+                    "\nPayment is " + CURRENCY + " " + payment;
+            view.getOutput(output);
+        }
     }
 
     private Buyer getBuyer(String[] data) {
--- src/app/model/AppModel.java
+++ src/app/model/AppModel.java
@@ -1,9 +1,9 @@
 package app.model;
 
-import app.entity.Delivery;
 import app.entity.Purchase;
 
 public interface AppModel {
 
     double calcPayment(Purchase purchase);
+
 }
